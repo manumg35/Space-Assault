a80f3a200056fa344d3cb1f75079b3ed0815f5d0
Manuel Martinez Giner <manumg35@outlook.es>
2023-03-09 12:26:01 +0100

    Enemy hit and Canvas with Score

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,12 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
+    [SerializeField] GameObject hitVFX;
     [SerializeField] Transform parent;
 
     [SerializeField] int scorePerHit=15;
+
+    [SerializeField] int healthPoints = 8;
+
+    
+
 
     ScoreBoard scoreBoard;
 
@@ -18,8 +25,33 @@ public class Enemy : MonoBehaviour
     }
     void OnParticleCollision(GameObject other)
     {
-        ProcessHit();
-        KillEnemy();
+        ProcessHit(other);
+        
+        if (healthPoints < 1)
+        {
+            KillEnemy();
+        }
+        
+    }
+
+    void ProcessHit(GameObject obj)
+    {
+        Vector3 LocationOfHit = transform.position;
+        ParticleSystem laserParticles = obj.gameObject.GetComponent<ParticleSystem>();
+        List<ParticleCollisionEvent> collisionEvents = new List<ParticleCollisionEvent>();
+        int numParticleCollisions = laserParticles.GetCollisionEvents(this.gameObject, collisionEvents);
+
+        if (numParticleCollisions > 0)
+        {
+            LocationOfHit = collisionEvents[0].intersection;
+        }
+
+
+        GameObject vfx = Instantiate(hitVFX, LocationOfHit, Quaternion.identity);
+        vfx.transform.parent = parent;
+        healthPoints--;
+        scoreBoard.UpdateScore(scorePerHit);
+        
     }
 
     void KillEnemy()
@@ -29,8 +61,5 @@ public class Enemy : MonoBehaviour
         Destroy(gameObject);
     }
 
-    void ProcessHit()
-    {
-        scoreBoard.UpdateScore(scorePerHit);
-    }
+    
 }
